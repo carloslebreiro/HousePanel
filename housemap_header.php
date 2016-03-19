bf382d956183d9f28fa060bd0e0e10585c41c63b
Ken Washington <ken@kenw.com>
2016-03-19 18:09:40 -0400

    Added files via upload

---
+++ housemap_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head><title>Smart Motion Sensor Authorization</title>
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type">
+<link rel="stylesheet" type="text/css" href="housemap.css">
+<script type="text/javascript" src="/jquery-min.js"></script>
+<script type="text/javascript" src="housemap.js"></script>
+</head>
+<body>
+<table align="left" border="0" cellpadding="0" cellspacing="0" width="900">
+<tbody>
+<tr>
+<td><img height = "200px" src="https://d3abxbgmpfhmxi.cloudfront.net/assets/img/home-how-it-works-home-monitoring.1b50cecc.jpg"></td>
+</tr>
+<tr><td>
